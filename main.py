f6930c8cede8e08c2365b3ab84bc0141832722d8
Cactochan <85914469+mastercodermerwin@users.noreply.github.com>
2022-12-23 16:20:37 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,76 @@
+"""
+ImportDiscord (1.0.0)
+
+Can be used to download chat from a channel..
+All chat may not be saved..
+
+ : Use a Token of a user Who is in the server..
+"""
+
+from rich import print
+import requests as r
+import json
+import time
+import math
+import os
+
+
+print("[blue]Token : [/blue]", end="")
+token = input("")
+
+print("[blue]Channel ID : [/blue]", end="")
+channel_id = input("")
+
+print("[blue]Lmit : [/blue]", end="")
+limit = input("")
+
+print("[blue]Dump Data ? [<name>/no] [/blue]", end="")
+dump = input("")
+
+
+url = f"https://discord.com/api/v9/channels/{channel_id}/messages?limit={limit}"
+url_2 = f"https://discord.com/api/v9/channels/{channel_id}/messages?limit=100"
+appd_url = f"https://discord.com/api/v9/channels/1041260688972197888/messages?before=__before__&limit=100"
+headers = {
+        "authorization" : token,
+        "user-agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+        }
+
+
+if int(limit) <= 100:
+    res = r.get(url, headers = headers)
+    print(f"STATS : {res} ")
+    data_ = json.loads(res.text) 
+else:
+    times = math.ceil(int(limit)/100)-1
+    buffer = 100
+    res = r.get(url_2, headers = headers)
+    print(f"STATS : {res} ")
+    data_ = json.loads(res.text)        
+    time.sleep(0.5)
+    for e in range(0,times):
+        prev = data_[-1]["id"]
+        res = r.get(appd_url.replace("__before__",prev), headers = headers)
+        print(f"STATS : {res} ")
+        data_ += json.loads(res.text)
+        time.sleep(1)
+
+if dump != "no":
+    file =  open(dump,"x")
+    file.write(json.dumps(data_))
+    file.close()
+
+bigG = 0
+for e in data_:
+    a = len(e["author"]["username"])
+    if a  > bigG:
+        bigG = a
+print(bigG)
+data = []
+for e in data_:
+    if e["content"] != "":
+        data.append(e)
+        a_d = " "*(bigG-len(e["author"]["username"]))
+        print(f"[orange_red1] AUTHOr: {e['author']['username']}[/orange_red1]{a_d} [yellow1]MSg: {e['content'] }[/yellow1]")
+
+print(f"[green]Total msgs :  {len(data)}[/green]")
